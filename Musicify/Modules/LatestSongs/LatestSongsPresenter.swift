5e53dd33735c628039a8b3050ebe722b7fae3dbf
Hai Phan <hai@makehelsinki.com>
2019-01-02 15:37:22 +0200

    Implement MVP. Add tests accordingly

---
+++ Musicify/Modules/LatestSongs/LatestSongsPresenter.swift
@@ -0,0 +1,48 @@
+//
+//  LatestSongsPresenter.swift
+//  Musicify
+//
+//  Created by Hai Phan on 02/01/2019.
+//  Copyright © 2019 Hai Phan. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+protocol LatestSongPresenter {
+    var view: LatestSongsView? { get set }
+    
+    func loadLatestSongs()
+}
+
+class LatestSongPresenterImpl: LatestSongPresenter {
+    // MARK: - View
+    weak var view: LatestSongsView?
+    
+    // MARK: - Dependencies
+    fileprivate let musicifyAPIProvider: MoyaProvider<MusicifyAPI>
+    
+    init(musicifyAPIProvider: MoyaProvider<MusicifyAPI> = MoyaProvider<MusicifyAPI>(stubClosure: MoyaProvider.delayedStub(2))) {
+        self.musicifyAPIProvider = musicifyAPIProvider
+    }
+    
+    func loadLatestSongs() {
+        view?.startLoading()
+        musicifyAPIProvider.request(.latestSongs) { [weak self] (result) in
+            switch result {
+            case .success(let response):
+                let data = response.data
+                guard let songs = try? JSONDecoder().decode([Song].self, from: data) else {
+                    self?.view?.stopLoading()
+                    self?.view?.showError(message: "JSON parsing error")
+                    return
+                }
+                self?.view?.stopLoading()
+                self?.view?.showLatestSongs(songs: songs)
+            case .failure(let error):
+                self?.view?.stopLoading()
+                self?.view?.showError(message: "Connection error")
+            }
+        }
+    }
+}
