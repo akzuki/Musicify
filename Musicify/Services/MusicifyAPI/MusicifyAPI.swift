ba827cef1024f3e833fad9ac1c5621b4e853da64
Hai Phan <hai@makehelsinki.com>
2019-01-02 13:46:25 +0200

    Add mock data for latest songs API endpoint

--- Musicify/Services/MusicifyAPI/MusicifyAPI.swift
+++ Musicify/Services/MusicifyAPI/MusicifyAPI.swift
@@ -31,7 +31,44 @@ enum MusicifyAPI: TargetType {
     }
     
     var sampleData: Data {
-        fatalError("Sample data is not available")
+        switch self {
+        case .latestSongs:
+            return (
+                """
+                [
+                    {
+                        "id": 1,
+                        "name": "Back To You",
+                        "artist": "Selena Gomez",
+                        "releaseYear": "2018",
+                        "thumbnailURL": "https://i.ytimg.com/vi/VY1eFxgRR-k/maxresdefault.jpg"
+                    },
+                    {
+                        "id": 2,
+                        "name": "Dance To This",
+                        "artist": "Troye Sivan ft Ariana Grande",
+                        "releaseYear": "2018",
+                        "thumbnailURL": "https://i.ytimg.com/vi/bhxhNIQBKJI/maxresdefault.jpg"
+                    },
+                    {
+                        "id": 3,
+                        "name": "Lost In Japan",
+                        "artist": "Shawn Mendes",
+                        "releaseYear": "2018",
+                        "thumbnailURL": "https://i.ytimg.com/vi/ycy30LIbq4w/maxresdefault.jpg"
+                    },
+                    {
+                        "id": 4,
+                        "name": "My My My!",
+                        "artist": "Troye Sivan",
+                        "releaseYear": "2018",
+                        "thumbnailURL": "https://i.ytimg.com/vi/k5TqNsr6YuQ/maxresdefault.jpg"
+                    }
+                ]
+                """
+                .data(using: .utf8)!
+            )
+        }
     }
     
     var task: Task {
